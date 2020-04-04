55b816d5a30441e2b9ef921026e85334929d7431
Ryan Harris <harris.ucla@gmail.com>
2020-04-03 17:35:59 -0700

    Change markdown generation for license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,15 @@
-function generateBadge(license) {
+function getLicenseUrl(license) {
   switch (license) {
     case "MIT":
-      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      return "https://opensource.org/licenses/MIT";
     case "APACHE 2.0":
-      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      return "https://opensource.org/licenses/Apache-2.0";
     case "GPL 3.0":
-      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      return "https://www.gnu.org/licenses/gpl-3.0";
     case "BSD 3":
-      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      return "https://opensource.org/licenses/BSD-3-Clause";
     case "MPL 2.0":
-      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      return "https://opensource.org/licenses/MPL-2.0";
     default:
       return "";
   }
@@ -19,7 +19,16 @@ function generateMarkdown(data) {
   return `
 # ${data.project}
 
-${generateBadge(data.license)}
+${
+  data.license !== "None"
+    ? `[![License ${
+        data.license
+      }](https://img.shields.io/badge/License-${data.license.replace(
+        " ",
+        "%20"
+      )}-blue.svg)](${getLicenseUrl(data.license)})`
+    : ""
+}
 
 ## Description
 
@@ -48,7 +57,11 @@ ${data.usage}
 
 ## License
 
-This project is licensed under the ${data.license} license.
+${
+  data.license !== "None"
+    ? `This project is licensed under the ${data.license} license.`
+    : "No License"
+}
 
 ## Contributing
 
